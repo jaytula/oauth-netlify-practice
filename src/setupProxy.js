75b73709d009da585878a78becd192431f6af756
Jay T <jaytula@gmail.com>
2020-09-20 11:48:19 -0700

    Setup webpack proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+console.log("setupProxy");
+
+module.exports = function(app) {
+  app.use(
+    '/.netlify/functions',
+    createProxyMiddleware({
+      target: 'http://localhost:9000',
+      changeOrigin: true,
+    })
+  );
+};
