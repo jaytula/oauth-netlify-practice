6319d647cd6d25c65810b3281e68db2dbdbab057
Jay T <jaytula@gmail.com>
2020-09-23 15:20:31 -0700

    Directly use NETLIFY environment variables

--- lambda/src/utils/auth.js
+++ lambda/src/utils/auth.js
@@ -9,14 +9,10 @@ const USER_PROFILE_URL = 'https://api.netlify.com/api/v1/user'
 const AUTHORIZATION_URL = 'https://app.netlify.com/authorize'
 const REDIRECT_URL = `${SITE_URL}/.netlify/functions/auth-callback`
 
-/* Env key name */
-const clientIdKey = 'NETLIFY_OAUTH_CLIENT_ID'
-const clientSecretKey = 'NETLIFY_OAUTH_CLIENT_SECRET'
-
 const config = {
   /* values set in terminal session or in netlify environment variables */
-  clientId: process.env[clientIdKey],
-  clientSecret: process.env[clientSecretKey],
+  clientId: process.env.NETLIFY_OAUTH_CLIENT_ID,
+  clientSecret: process.env.NETLIFY_OAUTH_CLIENT_SECRET,
   /* OAuth API endpoints */
   tokenHost: TOKEN_HOST,
   authorizePath: AUTHORIZATION_URL,
@@ -28,10 +24,10 @@ const config = {
 
 function authInstance(credentials) {
   if (!credentials.client.id) {
-    throw new Error(`MISSING REQUIRED ENV VARS. Please set ${clientIdKey}`)
+    throw new Error(`MISSING REQUIRED ENV VARS. Please set NETLIFY_OAUTH_CLIENT_ID`)
   }
   if (!credentials.client.secret) {
-    throw new Error(`MISSING REQUIRED ENV VARS. Please set ${clientSecretKey}`)
+    throw new Error(`MISSING REQUIRED ENV VARS. Please set NETLIFY_OAUTH_CLIENT_SECRET`)
   }
   return simpleOauth.create(credentials)
 }
