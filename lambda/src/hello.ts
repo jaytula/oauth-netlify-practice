669f3b77b3d0af0a85f0ebe4ae51921bec6c2f81
Jay T <jaytula@gmail.com>
2020-09-24 01:38:55 -0700

    Send back json in hello endpoint

--- lambda/src/hello.ts
+++ lambda/src/hello.ts
@@ -3,6 +3,12 @@ import { APIGatewayEvent } from "aws-lambda";
 export const handler = async (event: APIGatewayEvent) => {
   return {
     statusCode: 200,
-    body: "hello world of ts",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      message: 'hello',
+      version: process.version
+    })
   };
 };
