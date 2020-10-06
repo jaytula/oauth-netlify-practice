0cb2b8f23c2c80e72f086213106b7b3ca6ec84a7
Jay T <jaytula@gmail.com>
2020-10-06 10:08:17 -0700

    Create User model

---
+++ lambda/src/models/User.ts
@@ -0,0 +1,14 @@
+import { Schema, default as mongoose } from "mongoose";
+
+const userSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+  },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
