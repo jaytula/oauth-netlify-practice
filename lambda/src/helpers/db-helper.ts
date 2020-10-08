40a417af8667dfc6cf649f84be5422b7f4e8e79f
Jay T <jaytula@gmail.com>
2020-10-07 19:42:07 -0700

    Add options to mongoose.connect

--- lambda/src/helpers/db-helper.ts
+++ lambda/src/helpers/db-helper.ts
@@ -1,15 +1,17 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose, { Mongoose } from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 let cachedDb: Mongoose | null = null;
 
 export const connectToDatabase = (uri: string = MONGODB_URI) => {
-  if(cachedDb)  {
-    return Promise.resolve(cachedDb)
+  if (cachedDb) {
+    return Promise.resolve(cachedDb);
   }
 
-  return mongoose.connect(uri).then(db => {
-    cachedDb = db;
-    return cachedDb;
-  })
-}
+  return mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(db => {
+      cachedDb = db;
+      return cachedDb;
+    });
+};
