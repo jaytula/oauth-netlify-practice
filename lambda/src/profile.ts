8d7aa5a8459e0abce2fc76c972c3ae7dc4eed966
Jay T <jaytula@gmail.com>
2020-10-10 10:38:10 -0700

    Create lambda function skeleton at profile

---
+++ lambda/src/profile.ts
@@ -0,0 +1,13 @@
+import { APIGatewayEvent } from "aws-lambda";
+
+export const handler = async (event: APIGatewayEvent) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      message: 'profile endpoint',
+    })
+  };
+};
