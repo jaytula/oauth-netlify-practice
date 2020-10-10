c4fbefaf31f82070946ef9666e5dfd4053eea3ff
Jay T <jaytula@gmail.com>
2020-10-10 13:35:38 -0700

    Parsed jwt cookie

--- lambda/src/profile.ts
+++ lambda/src/profile.ts
@@ -1,13 +1,20 @@
-import { APIGatewayEvent } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import cookie from 'cookie';
 
-export const handler = async (event: APIGatewayEvent) => {
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const parsedCookie = cookie.parse(event.headers.cookie);
+  
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      message: 'profile endpoint',
-    })
+      message: "profile endpoint",
+      headers: event.headers,
+      parsedCookie
+    }),
   };
 };
