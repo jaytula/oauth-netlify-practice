03de9cee9b98949c398322253cc1e1692f883c9e
Jay T <jaytula@gmail.com>
2020-10-18 10:21:52 -0700

    Display user json in Toolbar

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../services/auth";
+import classes from './Toolbar.module.css';
 
 const Toolbar: React.FC = ({ children }) => {
-  return <div>
-    <Link to="/lwa">LWA</Link>{' '}
-    <Link to="/netlify-oauth">Netlify OAuth</Link>{' '}
-    <Link to="/profile">Profile</Link>
-  </div>;
+  const { getUser } = useAuth();
+
+  const user = getUser();
+
+  return (
+    <div className={classes.Toolbar}>
+      <div>
+        <Link to="/lwa">LWA</Link>{" "}
+        <Link to="/netlify-oauth">Netlify OAuth</Link>{" "}
+        <Link to="/profile">Profile</Link>
+      </div>
+      <div>
+        {JSON.stringify(user, null, 2)}
+      </div>
+    </div>
+  );
 };
 
 export default Toolbar;
