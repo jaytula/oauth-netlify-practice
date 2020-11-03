768e2f5dce937d15fe2ea90f069c2035129fd998
Jay T <jaytula@gmail.com>
2020-11-03 13:43:22 -0800

    Update Toolbar to display links according to authentication status

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import dayjs from "dayjs";
 
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../../services/auth";
 import classes from "./Toolbar.module.css";
 import { useInterval } from "../../utils/helpers";
-
-
 
 const Toolbar: React.FC = ({ children }) => {
   const { user, logout } = useAuth();
@@ -19,28 +17,30 @@ const Toolbar: React.FC = ({ children }) => {
     });
   };
 
-  const iat = dayjs.unix(Number(user.iat)); 
+  const iat = dayjs.unix(Number(user.iat));
 
   useInterval(() => {
     const exp = dayjs.unix(Number(user.exp));
 
-    setTimeLeft(exp.diff(dayjs(), 'second'));
-  }, 1000)
+    setTimeLeft(exp.diff(dayjs(), "second"));
+  }, 1000);
 
   return (
     <div className={classes.Toolbar}>
       <div>
-        <Link to="/lwa">LWA</Link>{" "}
-        <Link to="/netlify-oauth">Netlify OAuth</Link>{" "}
-        <Link to="/profile">Profile</Link>
+        {!user.email && (
+          <Fragment>
+            <Link to="/lwa">LWA</Link>{" "}
+            <Link to="/netlify-oauth">Netlify OAuth</Link>{" "}
+          </Fragment>
+        )}
+        {user.email && <Link to="/profile">Profile</Link>}
       </div>
       <div>
         {user.email ? (
           <div>
-            {user.email} | {user.userId} |{" "}
-            {iat.format("YYYY-MM-DDTHH:mm:ssZZ")} |{" "}
-            {timeLeft} |{" "}
-            <button onClick={onLogout}>Logout</button>
+            {user.email} | {user.userId} | {iat.format("YYYY-MM-DDTHH:mm:ssZZ")}{" "}
+            | {timeLeft} | <button onClick={onLogout}>Logout</button>
           </div>
         ) : null}
       </div>
