b62abc7eb59c75a57bda25978214ba7955751ac2
Jay T <jaytula@gmail.com>
2020-11-07 12:40:50 -0800

    Move authWithEmail to helpers/auth-helpers.ts

---
+++ lambda/src/helpers/auth-helpers.ts
@@ -0,0 +1,42 @@
+import { User } from "../models/User";
+import { connectToDatabase } from "./db-helper";
+import { createJwtPayload, createJwtCookieFromPayload, decodeJwtPayload } from "./jwt-helper";
+
+export const authWithEmail = async (email: string) => {
+  await connectToDatabase();
+
+  let existingUser = await User.findOne({ email });
+
+  if (!existingUser) {
+    const newUser = User.build({
+      email,
+    });
+
+    existingUser = await newUser.save();
+  }
+
+  if(!existingUser) {
+    throw new Error('existingUser should not be null');
+  }
+  
+  const jwtPayload = createJwtPayload(email, existingUser._id);
+  const jwtCookie = createJwtCookieFromPayload(jwtPayload);
+
+  const payload = decodeJwtPayload(jwtPayload);
+
+  const searchParams = new URLSearchParams();
+  searchParams.append('userId', existingUser._id);
+  searchParams.append('email', existingUser.email);
+  searchParams.append('iat', payload.iat.toString());
+  searchParams.append('exp', payload.exp.toString());
+
+  return {
+    statusCode: 302,
+    headers: {
+      "Content-Type": "application/json",
+      "Set-Cookie": jwtCookie,
+      "Location": `/profile?${searchParams.toString()}`
+    },
+    body: JSON.stringify({ existingUser, jwtCookie }, null, 2),
+  };
+}
--- lambda/src/netlify-auth-callback.ts
+++ lambda/src/netlify-auth-callback.ts
@@ -2,48 +2,7 @@ import querystring from "querystring";
 import { authorizationCodeClient, REDIRECT_URL } from "./utils/netlify-auth";
 import { getUser } from "./utils/netlify-api";
 import { APIGatewayEvent } from "aws-lambda";
-import { createJwtCookieFromPayload, createJwtPayload, decodeJwtPayload } from "./helpers/jwt-helper";
-import { connectToDatabase } from "./helpers/db-helper";
-import { User } from "./models/User";
-
-const authWithEmail = async (email: string) => {
-  await connectToDatabase();
-
-  let existingUser = await User.findOne({ email });
-
-  if (!existingUser) {
-    const newUser = User.build({
-      email,
-    });
-
-    existingUser = await newUser.save();
-  }
-
-  if(!existingUser) {
-    throw new Error('existingUser should not be null');
-  }
-  
-  const jwtPayload = createJwtPayload(email, existingUser._id);
-  const jwtCookie = createJwtCookieFromPayload(jwtPayload);
-
-  const payload = decodeJwtPayload(jwtPayload);
-
-  const searchParams = new URLSearchParams();
-  searchParams.append('userId', existingUser._id);
-  searchParams.append('email', existingUser.email);
-  searchParams.append('iat', payload.iat.toString());
-  searchParams.append('exp', payload.exp.toString());
-
-  return {
-    statusCode: 302,
-    headers: {
-      "Content-Type": "application/json",
-      "Set-Cookie": jwtCookie,
-      "Location": `/profile?${searchParams.toString()}`
-    },
-    body: JSON.stringify({ existingUser, jwtCookie }, null, 2),
-  };
-}
+import { authWithEmail } from "./helpers/auth-helpers";
 
 /* Function to handle netlify auth callback */
 export const handler = async (event: APIGatewayEvent) => {
