e7d46e53aa1335e0ecbae6ba2d484616dd66718c
Jay T <jaytula@gmail.com>
2020-11-16 08:54:56 -0800

    Provide Logout button in RefreshModal

--- src/components/RefreshModal/RefreshModal.tsx
+++ src/components/RefreshModal/RefreshModal.tsx
@@ -1,16 +1,19 @@
 import dayjs from 'dayjs';
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import { useAuth } from '../../providers/auth-provider';
 import { useInterval } from '../../utils/helpers';
 import classes from './RefreshModal.module.css';
 
 const RefreshModal = () => {
   const [secondsLeft, setSecondsLeft] = useState(0);
-  const { user, refresh } = useAuth();
+  const { user, refresh, logout } = useAuth();
+  const history = useHistory();
 
   useInterval(() => {
     const exp = dayjs.unix(user.exp);
     const updatedSecondsLeft = exp.diff(dayjs(), 'second');
+    
     setSecondsLeft(updatedSecondsLeft);
   }, 1000);
 
@@ -18,11 +21,18 @@ const RefreshModal = () => {
     refresh();
   };
 
+  const onLogout = () => {
+    logout(() => {
+      history.replace("/");
+    })
+  }
+
   return user.email && secondsLeft >= 1 && secondsLeft <= 90 ? (
     <div className={classes.RefreshModal}>
       <div className={classes.RefreshModalContent}>
         You will be logged out in: {secondsLeft} seconds
         <button onClick={onRefresh}>Refresh</button>
+        <button onClick={onLogout}>Logout</button>
       </div>
     </div>
   ) : null;
