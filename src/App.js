0cdd8a4f5cbc44d41b24b7fcbabd8095f2d704dd
Jay T <jaytula@gmail.com>
2020-11-23 12:10:06 -0800

    Use RefreshModal from @groundearth/auth-utils

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from "./components/Layout/Layout";
 import LoginWithAmazon from "./components/Pages/LoginWithAmazon";
 import NetlifyApp from "./components/Pages/NetlfiyApp/NetlifyApp";
 import ProfilePage from "./components/Pages/ProfilePage";
-import RefreshModal from "./components/RefreshModal/RefreshModal";
+import { RefreshModal } from "@groundearth0/auth-utils";
 import { AuthProvider, useAuth } from "./providers/auth-provider";
 
 const AppRouter = () => {
