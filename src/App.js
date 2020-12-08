5b2ad6353c73897646a036e83742b22074487423
Jay T <jaytula@gmail.com>
2020-12-08 12:11:37 -0800

    Discovered result of useHistory is undefined

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import LoginWithAmazon from "./components/Pages/LoginWithAmazon";
 import NetlifyApp from "./components/Pages/NetlfiyApp/NetlifyApp";
 import ProfilePage from "./components/Pages/ProfilePage";
 import { RefreshModal } from "@groundearth0/auth-utils";
-import '@groundearth0/auth-utils/dist/index.css';
+import "@groundearth0/auth-utils/dist/index.css";
 import { AuthProvider, useAuth } from "./providers/auth-provider";
 
 const AppRouter = () => {
@@ -15,10 +15,37 @@ const AppRouter = () => {
 
   const onLogout = () => {
     logout(() => {
+      console.log({history})
       history.replace("/");
     });
   };
   checkUser();
+
+  const onRefreshData = (data) => {
+      console.log(data);
+      console.log('abc');
+
+      // Need to redirect to `/profile` which is the ProfilePage component
+      // Needed queryParams are: userId, email, iat, exp
+
+      const urlParams = new URLSearchParams();
+      urlParams.set("userId", data.userId);
+      urlParams.set("email", data.email);
+      urlParams.set("iat", data.iat);
+      urlParams.set("exp", data.exp);
+
+      const qs = urlParams.toString();
+
+      // TODO: why is history undefined?
+      console.log(history);
+            // history.push(`/profile?${qs}`);
+
+  }
+
+  const onRefresh = () => {
+    console.log("TODO: onRefresh");
+    refresh().then(onRefreshData);
+  };
 
   return (
     <BrowserRouter>
@@ -27,7 +54,7 @@ const AppRouter = () => {
           enabled={!!user.email}
           exp={user.exp}
           onLogout={onLogout}
-          refresh={refresh}
+          refresh={onRefresh}
           secondsRemaining={460}
         />
         <div className={classes.App}>
