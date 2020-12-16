67b42ac83a838a483091736c276feea545c96bd3
Jay T <jaytula@gmail.com>
2020-12-16 07:54:53 -0800

    Add button in GoogleOauthApp

--- src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
+++ src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
 
 const GoogleOauthApp = () => {
+  const onClickHandler = () => {
+    console.log("onClickHandler");
+
+    // TODO: Figure out how to declare gapi
+    // https://mariusschulz.com/blog/declaring-global-variables-in-typescript
+    console.log(gapi);
+  };
   return (
     <div>
       <h2>Google Oauth App Demo</h2>
+
+      <button onClick={onClickHandler}>Login</button>
     </div>
   );
 };
