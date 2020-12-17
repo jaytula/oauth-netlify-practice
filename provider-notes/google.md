558ed5e5fef89a536773730f5f1b1ff4991fc9d6
Jay T <jaytula@gmail.com>
2020-12-17 08:17:52 -0800

    Get the Oauth client id in GoogleOAuthApp component

--- provider-notes/google.md
+++ provider-notes/google.md
@@ -7,7 +7,7 @@ https://developers.google.com/identity/protocols/oauth2#clientside
   - Select `OAUTH2 Client ID`
   - Fill Oauth consent screen stuff
   - Create Oauth credentials
-  - Place `GOOGLE_OAUTH_CLIENT_ID` and `GOOGLE_OAUTH_CLIENT_SECRET` as env vars
+  - Place `REACT_APP_GOOGLE_OAUTH_CLIENT_ID` and `GOOGLE_OAUTH_CLIENT_SECRET` as env vars
 
   ## Links
 
--- src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
+++ src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 
+const GOOGLE_OAUTH_CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
+
 const GoogleOauthApp = () => {
   const onClickHandler = () => {
     console.log("onClickHandler");
 
     // TODO: Figure out how to declare gapi
     // https://mariusschulz.com/blog/declaring-global-variables-in-typescript
-    console.log(gapi);
+    console.log({GOOGLE_OAUTH_CLIENT_ID});
   };
   return (
     <div>
