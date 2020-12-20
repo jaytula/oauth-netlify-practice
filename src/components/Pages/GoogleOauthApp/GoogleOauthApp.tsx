edd81e5df49c9986be0ac204f9456caae9acd727
Jay T <jaytula@gmail.com>
2020-12-20 09:51:04 -0800

    Add several more links on Google Oauth2
    Looks like react-google-login is the way to go

--- src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
+++ src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
@@ -2,15 +2,19 @@ import * as React from "react";
 
 const GOOGLE_OAUTH_CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
 
+
 const GoogleOauthApp = () => {
+  React.useEffect(() => {
+    // TODO: Google Sign-In JavaScript client
+    // Reference: https://developers.google.com/identity/sign-in/web/reference
+    // Article: https://developers.google.com/identity/sign-in/web/sign-in
+    // https://www.npmjs.com/package/react-google-login
+    console.log(gapi)
+  }, [])
   const onClickHandler = () => {
     console.log("onClickHandler");
 
-    // TODO: Hello, GAPI Article
-    // https://medium.com/google-cloud/gapi-the-google-apis-client-library-for-browser-javascript-5896b12dbbd5
-    // TODO: Figure out how to declare gapi
-    // https://mariusschulz.com/blog/declaring-global-variables-in-typescript
-    console.log(gapi)
+
     console.log({GOOGLE_OAUTH_CLIENT_ID});
   };
   return (
