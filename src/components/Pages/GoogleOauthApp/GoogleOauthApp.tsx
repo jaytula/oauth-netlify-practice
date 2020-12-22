94945e7126a53c9e29bd95554eeb8a78fa005db1
Jay T <jaytula@gmail.com>
2020-12-22 09:32:17 -0800

    Add untested GoogleLogin button

--- src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
+++ src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
@@ -1,27 +1,32 @@
 import * as React from "react";
+import GoogleLogin from "react-google-login";
 
-const GOOGLE_OAUTH_CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID;
-
+const GOOGLE_OAUTH_CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string
 
 const GoogleOauthApp = () => {
-  React.useEffect(() => {
-    // TODO: Google Sign-In JavaScript client
-    // Reference: https://developers.google.com/identity/sign-in/web/reference
-    // Article: https://developers.google.com/identity/sign-in/web/sign-in
-    // https://www.npmjs.com/package/react-google-login
-    console.log(gapi)
-  }, [])
-  const onClickHandler = () => {
-    console.log("onClickHandler");
+  // TODO: Google Sign-In JavaScript client
+  // Reference: https://developers.google.com/identity/sign-in/web/reference
+  // Article: https://developers.google.com/identity/sign-in/web/sign-in
+  // https://www.npmjs.com/package/react-google-login
 
+  const onSuccess = (resp: any) => {
+    console.log({resp});
+  }
 
-    console.log({GOOGLE_OAUTH_CLIENT_ID});
-  };
+  const onFailure = (err: any) => {
+    console.log({err});
+  }
   return (
     <div>
       <h2>Google Oauth App Demo</h2>
 
-      <button onClick={onClickHandler}>Login</button>
+      <GoogleLogin
+        clientId={GOOGLE_OAUTH_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy="single_host_origin"
+      />
     </div>
   );
 };
