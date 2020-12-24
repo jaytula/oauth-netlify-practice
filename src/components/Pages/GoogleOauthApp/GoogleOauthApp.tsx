00c1f1ad2b582af0ec06e6b66b6331075f2f2ac1
Jay T <jaytula@gmail.com>
2020-12-24 12:39:09 -0800

    Type onSuccess function correctly

--- src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
+++ src/components/Pages/GoogleOauthApp/GoogleOauthApp.tsx
@@ -1,7 +1,44 @@
 import * as React from "react";
-import GoogleLogin from "react-google-login";
+import GoogleLogin, {
+  GoogleLoginProps,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+  UseGoogleLoginResponse,
+} from "react-google-login";
 
-const GOOGLE_OAUTH_CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID || "";
+const GOOGLE_OAUTH_CLIENT_ID =
+  process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID || "";
+
+interface OauthResponse {
+  Ca: string;
+  accessToken: string;
+  googleId: string;
+  profileObj: {
+    email: string;
+    familyName: string;
+    givenName: string;
+    googleId: string;
+    imageUrl: string;
+    name: string;
+  };
+  tokenId: string;
+  tokenObj: {
+    access_token: string;
+    expires_at: number;
+    expires_in: number;
+    first_issued_at: number;
+    id_token: string;
+    idbId: string;
+    login_hint: string;
+    scope: string;
+    session_state: {
+      extraQueryParams: {
+        authuser: number;
+      };
+    };
+    token_type: string;
+  };
+}
 
 const GoogleOauthApp = () => {
   // TODO: Google Sign-In JavaScript client
@@ -9,13 +46,15 @@ const GoogleOauthApp = () => {
   // Article: https://developers.google.com/identity/sign-in/web/sign-in
   // https://www.npmjs.com/package/react-google-login
 
-  const onSuccess = (resp: any) => {
-    console.log({resp});
-  }
+  const onSuccess: (
+    response: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => void = (response) => {
+    console.log(response);
+  };
 
   const onFailure = (err: any) => {
-    console.log({err});
-  }
+    console.log({ err });
+  };
   return (
     <div>
       <h2>Google Oauth App Demo</h2>
