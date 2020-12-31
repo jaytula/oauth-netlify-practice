c23c5a609914a5d537b6eb6fe05ccb46692ff82b
Jay T <jaytula@gmail.com>
2020-12-31 09:03:45 -0800

    Verify id_token using OAuth2Client from google-auth-library

--- lambda/src/google-login.ts
+++ lambda/src/google-login.ts
@@ -1,9 +1,28 @@
 import { APIGatewayEvent } from "aws-lambda";
+import {OAuth2Client} from 'google-auth-library';
+
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID as string;
 
 export const handler = async (event: APIGatewayEvent) => {
   const { queryStringParameters } = event;
 
-  console.log({queryStringParameters});
+  if(!queryStringParameters || !queryStringParameters.id_token) {
+    return {
+      statusCode: 500,
+    }
+  }
+  const id_token = queryStringParameters.id_token as string;
+
+  const client = new OAuth2Client(CLIENT_ID);
+  const ticket = await client.verifyIdToken({idToken: id_token, audience: CLIENT_ID})
+  const payload = ticket.getPayload();
+  if(!payload || !('sub' in payload)) {
+    return {
+      statusCode: 500,
+    }
+  }
+  
+  const userid = payload['sub']
 
   // TODO: Verify integrity of ID Token
   // https://developers.google.com/identity/sign-in/web/backend-auth
@@ -13,6 +32,6 @@ export const handler = async (event: APIGatewayEvent) => {
       'Content-Type': 'application/json'
     },
 
-    body: JSON.stringify(queryStringParameters)
+    body: JSON.stringify({userid})
   }
 };
